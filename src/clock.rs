99eee813cf3e64a855a61974f113678779a2bc14
cjsindt <connor.sindt@gmail.com>
2024-10-04 19:14:49 +0000

    added clock

---
+++ src/clock.rs
@@ -0,0 +1,46 @@
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+
+pub struct Clock {
+    total_cycles: Arc<Mutex<u64>>,
+    clock_speed: Arc<u32>,
+}
+
+impl Clock {
+    pub fn new(speed: u32) -> Clock {
+        Clock {
+            total_cycles: Arc::new(Mutex::new(0)), // Initialize total_cycles
+            clock_speed: Arc::new(speed),
+        }
+    }
+
+    // start the clock in a separate thread
+    pub fn start(&self) {
+        let total_cycles = Arc::clone(&self.total_cycles);
+        let clock_speed = Arc::clone(&self.clock_speed);
+
+        thread::spawn(move || {
+            let period = 1_000_000_000u64 / (*clock_speed as u64); // Convert Hz to nanoseconds
+            
+            let mut last_time = Instant::now();
+            let nanoseconds_per_cycle = Duration::from_nanos(period);
+
+            // busy-wait loop to emulate timing
+            loop{
+                while Instant::now().duration_since(last_time) < nanoseconds_per_cycle {
+                    thread::yield_now();
+                }
+
+                let mut cycles = total_cycles.lock().unwrap();
+                *cycles += 1;
+                last_time += nanoseconds_per_cycle;
+            }
+        });
+    }
+
+    pub fn get_total_cycles(&self) -> u64 {
+        let cycles = self.total_cycles.lock().unwrap();
+        *cycles
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+mod clock;
 
+use clock::Clock;
+use std::time::Duration;
+use std::thread;
+
+const CPU_SPEED: u32 = 4_190_000;   // cpu clock speed in Hz
 
 fn main() {
-    println!("Hello, world!");
+    // setup clock
+    let cpu_clock = Clock::new(CPU_SPEED);
+    cpu_clock.start();
+
+    thread::sleep(Duration::from_secs(2));
+
+    println!("Total cycles: {}", cpu_clock.get_total_cycles());
 }
